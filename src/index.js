4503c0e52fefbe879f8f1e6bfbd3a81df03b1579
Francesco Michelini <hello@francescomichelini.com>
2021-02-28 14:44:30 +0100

    Add basic game mechanics

--- src/index.js
+++ src/index.js
@@ -3,50 +3,59 @@ import {
   Scene,
   WebGLRenderer,
   PerspectiveCamera,
-  BoxGeometry,
-  MeshStandardMaterial,
-  ShaderMaterial,
+  CylinderGeometry,
+  TorusGeometry,
+  SphereGeometry,
+  // MeshStandardMaterial,
+  MeshBasicMaterial,
+  // ShaderMaterial,
   Mesh,
   PointLight,
   Color,
-  Clock
+  Clock,
+  BackSide,
+  Group
 } from 'three'
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-
-// Remove this if you don't need to load any 3D model
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
-
 import Tweakpane from 'tweakpane'
 
 class App {
   constructor(container) {
     this.container = document.querySelector(container)
 
+    this.ui = {
+      score: document.querySelector('[data-ui-score]')
+    }
+
+    this.state = {
+      score: 0
+    }
+
     this._resizeCb = () => this._onResize()
+    this._mousemoveCb = e => this._onMousemove(e)
   }
 
   init() {
     this._createScene()
     this._createCamera()
     this._createRenderer()
-    this._createBox()
-    this._createShadedBox()
     this._createLight()
     this._createClock()
+    this._createTube()
+    this._createPlayer()
+    this._createBalls()
+    this._spawnBall()
     this._addListeners()
     this._createControls()
     this._createDebugPanel()
 
-    this._loadModel().then(() => {
-      this.renderer.setAnimationLoop(() => {
-        this._update()
-        this._render()
-      })
-
-      console.log(this)
+    this.renderer.setAnimationLoop(() => {
+      this._update()
+      this._render()
     })
+
+    console.log(this)
   }
 
   destroy() {
@@ -57,11 +66,29 @@ class App {
   _update() {
     const elapsed = this.clock.getElapsedTime()
 
-    this.box.rotation.y = elapsed
-    this.box.rotation.z = elapsed*0.6
+    this.balls.children.forEach(ball => {
+      ball.position.z += 0.05
 
-    this.shadedBox.rotation.y = elapsed
-    this.shadedBox.rotation.z = elapsed*0.6
+      // Check the Z coordinate of the ball compared to the player
+      if (ball.position.z > this.player.position.z) {
+        // Compare ball's theta to player's theta
+        if (
+            ball.userData.theta <= this.player.userData.theta + 0.1 &&
+            ball.userData.theta >= this.player.userData.theta - 0.1
+            ) {
+          this.balls.remove(ball)
+          this.state.score++
+          this.ui.score.textContent = this.state.score
+          this._spawnBall()
+        }
+      }
+
+      // Remove the skipped ball when it goes out of view
+      if (ball.position.z > 13) {
+        this.balls.remove(ball)
+        this._spawnBall()
+      }
+    })
   }
 
   _render() {
@@ -74,7 +101,7 @@ class App {
 
   _createCamera() {
     this.camera = new PerspectiveCamera(75, this.container.clientWidth / this.container.clientHeight, 0.1, 100)
-    this.camera.position.set(-4, 4, 10)
+    this.camera.position.set(0, 0, 14)
   }
 
   _createRenderer() {
@@ -92,92 +119,9 @@ class App {
   }
 
   _createLight() {
-    this.pointLight = new PointLight(0xff0055, 500, 100, 2)
-    this.pointLight.position.set(0, 10, 13)
+    this.pointLight = new PointLight(0xff0055, 50, 100, 2)
+    this.pointLight.position.set(0, 1, 3)
     this.scene.add(this.pointLight)
-  }
-
-  /**
-   * Create a box with a PBR material
-   */
-  _createBox() {
-    const geometry = new BoxGeometry(1, 1, 1, 1, 1, 1)
-
-    const material = new MeshStandardMaterial({
-      color: 0xffffff,
-      metalness: 0.7,
-      roughness: 0.35
-    })
-
-    this.box = new Mesh(geometry, material)
-
-    this.box.scale.x = 4
-    this.box.scale.y = 4
-    this.box.scale.z = 4
-
-    this.box.position.x = -5
-
-    this.scene.add(this.box)
-  }
-
-  /**
-   * Create a box with a custom ShaderMaterial
-   */
-  _createShadedBox() {
-    const geometry = new BoxGeometry(1, 1, 1, 1, 1, 1)
-
-    const material = new ShaderMaterial({
-      vertexShader: require('./shaders/sample.vertex.glsl'),
-      fragmentShader: require('./shaders/sample.fragment.glsl'),
-      transparent: true
-    })
-
-    this.shadedBox = new Mesh(geometry, material)
-
-    this.shadedBox.scale.x = 4
-    this.shadedBox.scale.y = 4
-    this.shadedBox.scale.z = 4
-
-    this.shadedBox.position.x = 5
-
-    this.scene.add(this.shadedBox)
-  }
-
-  /**
-   * Load a 3D model and append it to the scene
-   */
-  _loadModel() {
-    return new Promise(resolve => {
-      this.loader = new GLTFLoader()
-
-      const dracoLoader = new DRACOLoader()
-      dracoLoader.setDecoderPath('/')
-
-      this.loader.setDRACOLoader(dracoLoader)
-
-      this.loader.load('./model.glb', gltf => {
-        const mesh = gltf.scene.children[0]
-
-        mesh.scale.x = 4
-        mesh.scale.y = 4
-        mesh.scale.z = 4
-
-        mesh.position.z = 5
-
-        const material = new ShaderMaterial({
-          vertexShader: require('./shaders/sample.vertex.glsl'),
-          fragmentShader: require('./shaders/sample.fragment.glsl'),
-          transparent: true,
-          wireframe: true
-        })
-
-        mesh.material = material
-
-        this.scene.add(mesh)
-
-        resolve()
-      })
-    })
   }
 
   _createControls() {
@@ -199,24 +143,6 @@ class App {
     })
 
     /**
-     * Box configuration
-     */
-    const boxFolder = this.pane.addFolder({ title: 'Box' })
-
-    boxFolder.addInput(this.box.scale, 'x', { label: 'Width', min: 1, max: 8 })
-      .on('change', e => this.shadedBox.scale.x = e.value)
-
-    boxFolder.addInput(this.box.scale, 'y', { label: 'Height', min: 1, max: 8 })
-      .on('change', e => this.shadedBox.scale.y = e.value)
-
-    boxFolder.addInput(this.box.scale, 'z', { label: 'Depth', min: 1, max: 8 })
-      .on('change', e => this.shadedBox.scale.z = e.value)
-
-    boxFolder.addInput(this.box.material, 'metalness', { label: 'Metallic', min: 0, max: 1 })
-
-    boxFolder.addInput(this.box.material, 'roughness', { label: 'Roughness', min: 0, max: 1 })
-
-    /**
      * Light configuration
      */
     const lightFolder = this.pane.addFolder({ title: 'Light' })
@@ -232,12 +158,69 @@ class App {
     lightFolder.addInput(this.pointLight, 'intensity', { label: 'Intensity', min: 0, max: 1000 })
   }
 
+  _createTube() {
+    const geom = new CylinderGeometry(5, 5, 30, 48, 1, true)
+    const mat = new MeshBasicMaterial({
+      color: 0x090909,
+      side: BackSide
+    })
+
+    this.tube = new Mesh(geom, mat)
+
+    this.tube.rotation.x = Math.PI*-0.5
+
+    this.scene.add(this.tube)
+  }
+
+  _createPlayer() {
+    const geom = new TorusGeometry(0.8, 0.15, 12, 50, Math.PI*2)
+    const mat = new MeshBasicMaterial({ color: 0x00ffff })
+
+    this.player = new Mesh(geom, mat)
+
+    this.player.position.z = 4.5
+
+    this.scene.add(this.player)
+  }
+
+  _spawnBall() {
+    const geom = this.ballGeometry
+    const mat = Math.random() > 0.5 ?
+                  this.ballBlackMaterial :
+                  this.ballWhiteMaterial
+
+    const mesh = new Mesh(geom, mat)
+
+    const theta = Math.random()*Math.PI*2 - Math.PI
+
+    mesh.userData.theta = theta
+    // console.log(theta)
+
+    const x = Math.cos(theta) * 4
+    const y = Math.sin(theta) * 4
+
+    mesh.position.set(x, y, -4)
+
+    this.balls.add(mesh)
+  }
+
+  _createBalls() {
+    this.balls = new Group()
+    this.scene.add(this.balls)
+
+    this.ballGeometry = new SphereGeometry(0.5, 16, 16)
+
+    this.ballBlackMaterial = new MeshBasicMaterial({ color: 0x404040 })
+    this.ballWhiteMaterial = new MeshBasicMaterial({ color: 0xf1f2f3 })
+  }
+
   _createClock() {
     this.clock = new Clock()
   }
 
   _addListeners() {
     window.addEventListener('resize', this._resizeCb, { passive: true })
+    this.container.addEventListener('mousemove', this._mousemoveCb, { passive: true })
   }
 
   _removeListeners() {
@@ -248,6 +231,23 @@ class App {
     this.camera.aspect = this.container.clientWidth / this.container.clientHeight
     this.camera.updateProjectionMatrix()
     this.renderer.setSize(this.container.clientWidth, this.container.clientHeight)
+  }
+
+  _onMousemove(e) {
+    const centerX = this.container.clientWidth*0.5
+    const centerY = this.container.clientHeight*0.5
+
+    const deltaX = e.clientX - centerX
+    const deltaY = e.clientY - centerY
+
+    const theta = Math.atan2(-deltaY, deltaX)
+    this.player.userData.theta = theta
+
+    const playerX = Math.cos(theta) * 4
+    const playerY = Math.sin(theta) * 4
+
+    this.player.position.x = playerX
+    this.player.position.y = playerY
   }
 }
 
